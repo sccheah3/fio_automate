1da4596e5c19edd48b53b37f025a90777e48d515
Stanley Cheah <sccheah3@gmail.com>
2020-07-31 14:05:17 -0700

    handle autopxe and ipmitool reset

--- fio_chart_project/fio_chart/forms.py
+++ fio_chart_project/fio_chart/forms.py
@@ -5,6 +5,8 @@ class UploadFioLogForm(forms.Form):
 
 class AutomateFioForm(forms.Form):
 	bmc_ip = forms.CharField()
+	bmc_username = forms.CharField()
+	bmc_password = forms.CharField()
 	lan_mac = forms.CharField()
 	cburn_img = forms.CharField()
 	burnin_dir = forms.CharField()
--- fio_chart_project/fio_chart/views.py
+++ fio_chart_project/fio_chart/views.py
@@ -6,6 +6,10 @@ from .forms import UploadFioLogForm, AutomateFioForm
 from . import chart_fio
 
 import io
+import subprocess
+import time
+
+RC_URL = '172.16.118.50/fio_automate_staging.sh'
 
 
 def upload_fio_log(request):
@@ -28,12 +32,25 @@ def automate_fio_test_and_chart(request):
 		form = AutomateFioForm(request.POST)
 
 		if form.is_valid():
-			print(form.cleaned_data)
-
+			set_autopxe(form.cleaned_data['lan_mac'], form.cleaned_data['cburn_img'], form.cleaned_data['burnin_dir'])
+			reset_server(form.cleaned_data['bmc_ip'], form.cleaned_data['bmc_username'], form.cleaned_data['bmc_password'])
+			return HttpResponse('submitted')
 
 	form = AutomateFioForm()
 	return render(request, 'fio_chart/automate_drive_benchmark_form.html', {'form': form})
 
+
+# can try to convert this to pycurl later. currently creating subproc with curl cmd
+def set_autopxe(lan_mac, cburn_img, burnin_dir):
+	command = "%s %s %s" %(cburn_img, RC_URL, burnin_dir)
+	subprocess.Popen(['curl', '-X', 'POST', '-F', 'command=%s'%(command), '-F', 'address=%s'%(lan_mac), '-F', 'action=Update', \
+					'172.16.0.3/cgi-bin/autopxe.php'])
+
+# dont want to do reset because when system is initial off state, will not turn on
+def reset_server(bmc_ip, bmc_username, bmc_password):
+	subprocess.Popen(['ipmitool', '-U', bmc_username, '-P', bmc_password, '-H', 'bmc_ip', 'power', 'off'])
+	time.sleep(10)
+	subprocess.Popen(['ipmitool', '-U', bmc_username, '-P', bmc_password, '-H', 'bmc_ip', 'power', 'on'])
 
 #class DriveViewSet(viewsets.ModelViewSet):
 #    queryset = DriveBenchmark.objects.all()
