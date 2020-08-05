a745567d6bf92c794487f8fc17c7b16229f6397c
Stanley Cheah <sccheah3@gmail.com>
2020-08-05 12:51:28 -0700

    menu for site navigation, customize jobfiles for spindle drives and ssds ramptime

--- cburn_scripts/create_fio_jobfile.py
+++ cburn_scripts/create_fio_jobfile.py
@@ -4,12 +4,34 @@
 
 import os
 import sys
+import subprocess
+import re
 
 SEQ_JOBFILE = "/root/fio_jobfile/seq.fio"
 RAND_JOBFILE = "/root/fio_jobfile/rand.fio"
 
 BLOCK_SIZES = ["1k", "2k", "4k", "8k", "16k", "32k", "64k", "128k", "256k", "512k", "1M", "2M"]
 
+# dict that holds value 1 if spindle and 0 for ssd
+rot_dict = {}
+
+val = subprocess.Popen(['/usr/bin/lsblk', '-d', '-o', 'name,rota'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+match = re.findall(r'(sd[a-zA-Z]+)[\s\t]+(\d)\n', val.stdout.read().decode('utf-8'), re.IGNORECASE)
+
+for m in match:
+		rot_dict[m[0]] = int(m[1])
+
+val = subprocess.Popen(['/usr/bin/lsblk', '-d', '-o', 'name,rota'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+match = re.findall(r'(nvme\d+n1)[\s\t]+(\d)\n', val.stdout.read().decode('utf-8'), re.IGNORECASE)
+for m in match:
+	rot_dict[m[0]] = int(m[1])
+
+def get_ramptime(dev_name):
+	# 0 - is for ssd values
+	if rot_dict[dev_name] == 0:
+		return 0
+	else:
+		return 10
 
 os.makedirs(os.path.dirname(SEQ_JOBFILE), exist_ok=True)
 
@@ -19,8 +41,7 @@ with open(SEQ_JOBFILE, 'w+') as s_jobfile:
 	s_jobfile.write("direct=1\n")
 	s_jobfile.write("buffered=0\n")
 	s_jobfile.write("ioengine=libaio\n")
-	s_jobfile.write("ramp_time=0\n")
-	s_jobfile.write("runtime=15\n")
+	s_jobfile.write("runtime=300\n")
 	s_jobfile.write("group_reporting\n")
 	s_jobfile.write("iodepth=128\n")
 	s_jobfile.write("numjobs=8\n")
@@ -33,11 +54,11 @@ with open(SEQ_JOBFILE, 'w+') as s_jobfile:
 	for x in sys.argv[1:]:
 		dev_full_name = str(x)
 		dev_name = x.split('/')[2]
-
 		for bs in BLOCK_SIZES:
 			s_jobfile.write("[seq_read_%s_%s]\n" %(bs, dev_name))
 			s_jobfile.write("stonewall\n")
 			s_jobfile.write("rw=read\n")
+			s_jobfile.write("ramp_time=%s\n" %(str(get_ramptime(dev_name))))
 			s_jobfile.write("bs=%s\n" %(bs))
 			s_jobfile.write("filename=%s\n" %(dev_full_name))
 			s_jobfile.write("\n")
@@ -51,6 +72,7 @@ with open(SEQ_JOBFILE, 'w+') as s_jobfile:
 			s_jobfile.write("[seq_write_%s_%s]\n" %(bs, dev_name))
 			s_jobfile.write("stonewall\n")
 			s_jobfile.write("rw=read\n")
+			s_jobfile.write("ramp_time=%s\n" %(str(get_ramptime(dev_name))))
 			s_jobfile.write("bs=%s\n" %(bs))
 			s_jobfile.write("filename=%s\n" %(dev_full_name))
 			s_jobfile.write("\n")
@@ -65,8 +87,7 @@ with open(RAND_JOBFILE, 'w+') as s_jobfile:
 	s_jobfile.write("direct=1\n")
 	s_jobfile.write("buffered=0\n")
 	s_jobfile.write("ioengine=libaio\n")
-	s_jobfile.write("ramp_time=0\n")
-	s_jobfile.write("runtime=2\n")
+	s_jobfile.write("runtime=300\n")
 	s_jobfile.write("group_reporting\n")
 	s_jobfile.write("iodepth=32\n")
 	s_jobfile.write("numjobs=8\n")
@@ -82,6 +103,7 @@ with open(RAND_JOBFILE, 'w+') as s_jobfile:
 		s_jobfile.write("[rand_read_4k_%s]\n" %(dev_name))
 		s_jobfile.write("stonewall\n")
 		s_jobfile.write("rw=randread\n")
+		s_jobfile.write("ramp_time=%s\n" % (str(get_ramptime(dev_name))))
 		s_jobfile.write("bs=4k\n")
 		s_jobfile.write("filename=%s\n" %(dev_full_name))
 		s_jobfile.write("\n")
@@ -94,6 +116,7 @@ with open(RAND_JOBFILE, 'w+') as s_jobfile:
 		s_jobfile.write("[rand_write_4k_%s]\n" %(dev_name))
 		s_jobfile.write("stonewall\n")
 		s_jobfile.write("rw=randwrite\n")
+		s_jobfile.write("ramp_time=%s\n" % (str(get_ramptime(dev_name))))
 		s_jobfile.write("bs=4k\n")
 		s_jobfile.write("filename=%s\n" %(dev_full_name))
 		s_jobfile.write("\n")
--- fio_chart_project/fio_chart/urls.py
+++ fio_chart_project/fio_chart/urls.py
@@ -5,11 +5,11 @@ from fio_chart_project.fio_chart import views
 app_name = 'drive_benchmark'
 
 urlpatterns = [
-	path('drives/', views.view_drives),
+	path('drives/', views.view_drives, name="drives"),
 	path('drives/<int:drive_id>/', views.drive_detail, name='drive_detail'),
 	path('drives/performance_comparison/', views.performance_comparison, name='performance_comparison'),
-    path('chart_log/', views.upload_fio_log),
-	path('automate_fio_form/', views.automate_fio_test_and_chart),
-	path('upload_performance_data/', views.parse_and_save_performance),
+    path('chart_log/', views.upload_fio_log, name="upload_fio_logs"),
+	path('automate_fio_form/', views.automate_fio_test_and_chart, name="automate_fio"),
+	path('upload_performance_data/', views.parse_and_save_performance, name="parse_and_save"),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
